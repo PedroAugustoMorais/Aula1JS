2e4f2ed9556a8db3c6777a667f5eec6a473b4f0a
PedroAugustoMorais <164585950+PedroAugustoMorais@users.noreply.github.com>
2025-06-25 20:52:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-alert('Seja bem vindo ao jogo!');
+﻿alert('Seja bem-vindo ao nosso jogo!');
 
-let entrada = prompt("Digite um numero:");
-let numero = parseInt(entrada); 
+// Gera um número secreto aleatório entre 1 e 10
+let numeroSecreto = 5;
+console.log(numeroSecreto); // (Para testes, pode remover depois)
 
-if(Number.isInteger(numero)){
-  console.log("Origado, Voce digitou um inteiro");
-}else{
-  console.log("Por favor, digite um inteiro");
+// Variável para armazenar o chute do usuário
+let chute;
+let tentativas = 0; // Inicia o contador de tentativas
+
+while (chute != numeroSecreto) {
+  chute = parseInt(prompt('Escolha um número entre 1 e 10'));
+  tentativas++; // Soma +1 a cada tentativa
+
+  if (chute == numeroSecreto) {
+    alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} na tentativa ${tentativas}.`);
+  } else if (chute > numeroSecreto) {
+    alert(`O número secreto é menor que ${chute}. Tente novamente.`);
+  } else {
+    alert(`O número secreto é maior que ${chute}. Tente novamente.`);
+  }
 }
-
-
